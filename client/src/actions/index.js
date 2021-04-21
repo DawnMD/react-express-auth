89f09027f5e620fca5148bf243beb81e41f018b6
DawnMD <mainakdas104@gmail.com>
2021-04-21 14:30:53 +0530

    redirect, error catch

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,12 @@
-import { AUTH_USER } from "./types";
+import { AUTH_USER, AUTH_ERROR } from "./types";
 import axios from "axios";
 
-export const signup = (formProps) => async (dispatch) => {
-  const res = await axios.post("http://localhost:3090/signup", formProps);
-
-  dispatch({ type: AUTH_USER, payload: res.data.token });
+export const signup = (formProps, callback) => async (dispatch) => {
+  try {
+    const res = await axios.post("http://localhost:3090/signup", formProps);
+    dispatch({ type: AUTH_USER, payload: res.data.token });
+    callback();
+  } catch (error) {
+    dispatch({ type: AUTH_ERROR, payload: "Email already in use" });
+  }
 };
